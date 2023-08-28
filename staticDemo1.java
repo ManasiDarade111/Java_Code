c13f63a605b56bfafffe4a9cea602d628e858a14
ManasiDarade111 <manasidarade2002@gmail.com>
2023-08-28 23:37:09 +0530

    any msg

---
+++ staticDemo1.java
@@ -0,0 +1,12 @@
+class staticDemo1
+{
+    static {
+        System.out.println("In static block ");
+   
+    }
+    public static void main (String[] args){
+        System.out.println("In main ");
+
+    }
+}
+ 
